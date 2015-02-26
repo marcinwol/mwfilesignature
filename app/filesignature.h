8afd0e9bbac271ba470e607a2da19ebb9d9c4d15
marcinwol <wolskint@o2.pl>
2015-02-26 17:32:57 +0800

    Signature class and get_bin_signature function added.

--- app/filesignature.h
+++ app/filesignature.h
@@ -1,10 +1,58 @@
 #ifndef FILESIGNATURE_H
 #define FILESIGNATURE_H
 
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <vector>
+
+
+#ifndef BRACED_INIT_LIST
+    #define BRACED_INIT_LIST(...) {__VA_ARGS__}
+#endif
+
+#ifndef ADD_SIGNATURE
+    #define ADD_SIGNATURE(sig, b) KNOWN_IMG_SIGNATURES.emplace<vector<unsigned char>, string>(BRACED_INIT_LIST sig, b);
+#endif
+
+
+
 namespace mw {
 
+  using namespace std;
 
-}
+
+  vector<unsigned char>
+  get_bin_signature(const string & in_file, int length = 132);
+
+  struct Signature
+  {
+     vector<unsigned char> sig;
+     string img_type;
+
+     Signature(const vector<unsigned char> & sig_, const string & img_type_ = "");
+
+     bool operator < (const Signature & other) const;
+     bool operator == (const Signature & other) const;
+
+  };
+
+
+
+
+
+
+//     inline void
+//     init_signatures()
+//     {
+//      //   ADD_SIGNATURE((1,2,3,4), "JPGE");
+//      //   ADD_SIGNATURE((1,2,3,5), "JPFP");
+//      //   ADD_SIGNATURE((1,2,3),   "JPFP3");
+//       ;
+//     }
+
+
+ }
 
 
 #endif // FILESIGNATURE_H
--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <iomanip>
 
 #include "filesignature.h"
 
@@ -11,7 +12,48 @@ int main(int ac, char* av[])
 {
 
 
-    cout << "Test" << endl;
+  string a_path {"/mnt/e/DCIM/20140907_165341.jpg"};
+
+    int length=2;
+
+//  cin >> length;
+
+  vector<unsigned char> signature = mw::get_bin_signature(a_path, length);
+
+  vector<unsigned char> bytes {0xFF, 0xFF, 0xFD};
+
+  for (const unsigned char & v: signature)
+  {
+      cout << hex << setfill('0') << setw(2) << uppercase << static_cast<unsigned>(v) <<" ";
+  }
+
+  cout << endl;
+
+
+
+
+
+
+//    mw::KNOWN_IMG_SIGNATURES
+
+//    ADD_SIGNATURE((1,2,3,4), "JPGE");
+//    ADD_SIGNATURE((1,2,3,5), "JPFP");
+//    ADD_SIGNATURE((1,2,3),   "JPFP3");
+
+//    mw::Signature unknowns {{1,2,3,4}};
+
+//    set<mw::Signature>::const_iterator item = sig_set.find(unknowns);
+//    if (item != sig_set.end())
+//    {
+//        cout << "found " << (*item).img_type << endl;
+//    }
+
+
+
+  //cout << (s1 < s2) << endl;
+//
+
+  cout << "Hello World!" << endl;
 
     return 0;
 }
