7bb95f787b2924f974e7ebbd99b96d593d064083
marcin <marcin.wolski@curtin.edu.au>
2015-02-27 09:42:42 +0800

    NEF signature added.

--- app/main.cpp
+++ app/main.cpp
@@ -3,9 +3,84 @@
 #include <vector>
 #include <iomanip>
 
+#include <errno.h>
+#include <string.h>
+#include <fts.h>
+
 #include "filesignature.h"
 
 using namespace std;
+
+
+
+
+int dir_tree_scan(const string & in_path, vector<string> & found_paths)
+{
+
+    char * cstr = new char[in_path.length() + 1];
+    strcpy(cstr, in_path.c_str());
+
+    char * path[2] {cstr, nullptr};
+
+    size_t i {0};
+
+
+    FTS *tree = fts_open(path, FTS_NOCHDIR, 0);
+
+    if (!tree)
+    {
+        delete[] cstr;
+        perror("fts_open");
+        return 1;
+    }
+
+    FTSENT *node;
+
+    while ((node = fts_read(tree)))
+    {
+/*        if (node->fts_level > 0 && node->fts_name[0] == '.')
+        {
+            //fts_set(tree, node, FTS_SKIP);
+
+        }
+
+
+
+        else*/
+
+        if (node->fts_info & FTS_F)
+        {
+            if (i % 100 == 0)
+            {
+                cout  << "\r" << "Read " << i << " files "
+                      << "in " << in_path
+                      << flush;
+            }
+
+            ++i;
+
+           found_paths.emplace_back<string>(node->fts_path);
+        }
+    }
+
+    if (errno)
+    {
+        delete[] cstr;
+        perror("fts_read");
+        return 1;
+    }
+
+    if (fts_close(tree))
+    {
+        delete[] cstr;
+        perror("fts_close");
+        return 1;
+    }
+
+    delete[] cstr;
+    return 0;
+}
+
 
 
 int main(int ac, char* av[])
@@ -21,17 +96,33 @@ int main(int ac, char* av[])
   };
 
 
+ std::string in_folder = "/mnt/e/ImageDatabases/Englund_MOA_knee_xrays";
+
+
+
+ std::vector<string> found_files;
+
+ if (dir_tree_scan(in_folder, found_files) == 1)
+ {
+     cout << "some error occured";
+     return 1;
+ }
+
+
 
   mw::signature_set known_sigs = mw::get_known_signatures();
 
   set<int> sig_lengths = mw::get_known_signatures_lengths();
 
 
-  for (const string & a_path : paths)
+  size_t file_no {1};
+
+
+  for (const string & a_path : found_files)
   {
-      cout << a_path << endl;
+      cout << file_no << ": " << a_path << endl;
       vector<unsigned char> signature = mw::get_bin_signature(a_path);
-      cout << mw::get_signature_as_string(signature) << endl;
+ //     cout << mw::get_signature_as_string(signature) << endl;
 
       for (auto sig_length : sig_lengths)
       {
@@ -39,9 +130,9 @@ int main(int ac, char* av[])
           vector<unsigned char> v_partial(signature.begin(),
                                           signature.begin() + sig_length);
 
-          cout << sig_length <<": " << mw::get_signature_as_string(v_partial);
+   //       cout << sig_length <<": " << mw::get_signature_as_string(v_partial);
 
-          cout << endl;
+   //       cout << endl;
 
           mw::signature_set_iter si = known_sigs.find(v_partial);
 
@@ -52,8 +143,10 @@ int main(int ac, char* av[])
                    << endl;
               break;
           }
-
       }
+
+      ++file_no;
+
     }
 
     return 0;
